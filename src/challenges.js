073d2e2b49befd59fb157ad9eae368439f5a332c
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-08-10 10:22:23 -0300

    adiciona desafio 7, catAndMouse e corrige highestCount

--- src/challenges.js
+++ src/challenges.js
@@ -30,14 +30,17 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
+// eslint-disable-next-line sonarjs/cognitive-complexity
 function highestCount(numerosArray) {
   let maiorNumero = numerosArray[0];
   let numeroRepete = 0;
+
   for (let i = 0; i <= numerosArray.lenght; i += 1) {
     if (numerosArray[i] > maiorNumero) {
       maiorNumero = numerosArray[i];
     }
   }
+
   for (let i = 0; i <= numerosArray.lenght; i += 1) {
     if (maiorNumero === numerosArray[i]) {
       numeroRepete += 1;
@@ -47,8 +50,18 @@ function highestCount(numerosArray) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distCat1 = Math.abs(mouse - cat1);
+  let distCat2 = Math.abs(mouse - cat2);
+  let string;
+  if (distCat1 > distCat2) {
+    string = 'Cat1';
+  } else if (distCat2 > distCat1) {
+    string = 'Cat2';
+  } else if (distCat1 === distCat2) {
+    string = 'os gatos trombam e o rato foge';
+  }
+  return string;
 }
 
 // Desafio 8
